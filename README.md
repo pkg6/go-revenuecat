916170f44d7a3d8931dffbb7e6eb06f7600d21b3
zhiqiangwang <zhiqiangwang@aliyun.com>
2024-01-26 11:04:56 +0800

    feat: transactions

--- README.md
+++ README.md
@@ -18,25 +18,5 @@ client.GetOrCreateSubscriber("dev-0")
 
 ## Implementation List
 
-| Official interface address                                   | Function                                |
-| ------------------------------------------------------------ | --------------------------------------- |
-| [Get or Create Subscriber](https://www.revenuecat.com/reference/subscribers) | GetOrCreateSubscriber                   |
-| [Delete Subscriber](https://www.revenuecat.com/reference/delete-subscriber) | DeleteSubscriber                        |
-| [Get a list of projects](https://www.revenuecat.com/reference/list-projects) | GetAListOfProjects                      |
-| [Create a Purchase](https://www.revenuecat.com/reference/receipts) | CreateAPurchase                         |
-| [Get a list of apps](https://www.revenuecat.com/reference/list-apps) | GetAListOfApps                          |
-| [Get an app](https://www.revenuecat.com/reference/get-app)   | GetAnApp                                |
-| [Grant a Promotional Entitlement](https://www.revenuecat.com/reference/grant-a-promotional-entitlement) | GrantAPromotionalEntitlement            |
-| [Revoke Promotional Entitlements](https://www.revenuecat.com/reference/revoke-promotional-entitlements) | RevokePromotionalEntitlements           |
-| [Override a Customer's Current Offering](https://www.revenuecat.com/reference/override-offering) | OverrideACustomerSCurrentOffering       |
-| [Override a Customer's Current Offering](https://www.revenuecat.com/reference/delete-offering-override) | RemoveACustomerSCurrentOfferingOverride |
-| [Get Offerings](https://www.revenuecat.com/reference/get-offerings) | GetOfferings                            |
-|                                                              |                                         |
-|                                                              |                                         |
-|                                                              |                                         |
-|                                                              |                                         |
-|                                                              |                                         |
-|                                                              |                                         |
-|                                                              |                                         |
-|                                                              |                                         |
+[https://pkg.go.dev/github.com/pkg6/go-revenuecat](https://pkg.go.dev/github.com/pkg6/go-revenuecat)
 
--- transactions.go
+++ transactions.go
@@ -1,16 +1,21 @@
 package revenuecat
 
 import (
+	"fmt"
 	"net/http"
 )
 
 const (
-	transactionsPath = "/v1/receipts"
+	transactionsPath                          = "/v1/receipts"
+	googlePlayRefundAndRevokeSubscriptionPath = "/v1/subscribers/%s/subscriptions/%s/revoke"
+	googlePlayDeferASubscriptionPath          = "/v1/subscribers/%s/subscriptions/%s/defer"
+	googlePlayRefundAndRevokePurchasePath     = "/v1/subscribers/%s/transactions/%s/refund"
+	googlePlayCancelASubscriptionPath         = " /v1/subscribers/%s/subscriptions/%s/cancel"
 )
 
 type CreateAPurchaseRequest struct {
-	AppUserId                   string      `json:"app_user_id,omitempty"`
-	FetchToken                  string      `json:"fetch_token,omitempty"`
+	AppUserId                   string      `json:"app_user_id"`
+	FetchToken                  string      `json:"fetch_token"`
 	ProductId                   string      `json:"product_id,omitempty"`
 	Price                       float64     `json:"price,omitempty"`
 	Currency                    string      `json:"currency,omitempty"`
@@ -61,6 +66,70 @@ func (c *Client) CreateAPurchase(req *CreateAPurchaseRequest, platforms ...strin
 		transactionsPath,
 		req,
 		platform,
+		&resp,
+	)
+	return
+}
+
+// GooglePlayRefundAndRevokeSubscription
+//https://www.revenuecat.com/reference/revoke-a-google-subscription
+func (c *Client) GooglePlayRefundAndRevokeSubscription(appUserId, productIdentifier string) (resp *SubscriberResponse, err error) {
+	resp = new(SubscriberResponse)
+	err = c.call(
+		c.PublicAPIKey,
+		http.MethodPost,
+		fmt.Sprintf(googlePlayRefundAndRevokeSubscriptionPath, appUserId, productIdentifier),
+		nil,
+		"",
+		&resp,
+	)
+	return
+}
+
+type GooglePlayDeferASubscriptionRequest struct {
+	ExpiryTimeMs int64 `json:"expiry_time_msm"`
+}
+
+// GooglePlayDeferASubscription
+//https://www.revenuecat.com/reference/defer-a-google-subscription
+func (c *Client) GooglePlayDeferASubscription(appUserId, productIdentifier string, req *GooglePlayDeferASubscriptionRequest) (resp *SubscriberResponse, err error) {
+	resp = new(SubscriberResponse)
+	err = c.call(
+		c.PublicAPIKey,
+		http.MethodPost,
+		fmt.Sprintf(googlePlayDeferASubscriptionPath, appUserId, productIdentifier),
+		req,
+		"",
+		&resp,
+	)
+	return
+}
+
+// GooglePlayRefundAndRevokePurchase
+//https://www.revenuecat.com/reference/refund-a-google-purchase
+func (c *Client) GooglePlayRefundAndRevokePurchase(appUserId, storeTransactionIdentifier string) (resp *SubscriberResponse, err error) {
+	resp = new(SubscriberResponse)
+	err = c.call(
+		c.PublicAPIKey,
+		http.MethodPost,
+		fmt.Sprintf(googlePlayRefundAndRevokePurchasePath, appUserId, storeTransactionIdentifier),
+		nil,
+		"",
+		&resp,
+	)
+	return
+}
+
+// GooglePlayCancelASubscription
+//https://www.revenuecat.com/reference/cancel-a-google-subscription
+func (c *Client) GooglePlayCancelASubscription(appUserId, storeTransactionIdentifier string) (resp *SubscriberResponse, err error) {
+	resp = new(SubscriberResponse)
+	err = c.call(
+		c.PublicAPIKey,
+		http.MethodPost,
+		fmt.Sprintf(googlePlayCancelASubscriptionPath, appUserId, storeTransactionIdentifier),
+		nil,
+		"",
 		&resp,
 	)
 	return
